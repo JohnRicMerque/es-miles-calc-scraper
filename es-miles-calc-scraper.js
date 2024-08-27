cf4d28c7817c6839bbd31f6cf91ccfd505b4e9da
John Ric Merque <johnricmer@gmail.com>
2024-08-28 03:59:31 +0800

    plugins

--- es-miles-calc-scraper.js
+++ es-miles-calc-scraper.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const XLSX = require('xlsx');
 
 // Function to handle cookies window
